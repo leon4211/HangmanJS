d0e084b1c277943f957e24fa17e53c33e20703d1
Bonde.leon <leonbonde1@gmail.com>
2024-01-19 10:19:05 +0100

    alles bis auf server

--- src/Main.js
+++ src/Main.js
@@ -8,9 +8,20 @@ function getWord() {
     letters = secretWord.split('');
     for (i = 0; i < letters.length; i++) {
         $(".dashes span").text(letters.length);
-        answerArray[i] = " _ ";
+        answerArray[i] = " _ \n";
         $(".dashes div").append(answerArray[i]);
     }
+    $(".versuche div").html(versucheUebrig());
+}
+
+function versucheUebrig() {
+    var versucheGesamt = secretWord.length + secretWord.length + 2;
+    for (i = versucheGesamt; i >= 0; i--) {
+        versucheGesamt = versucheGesamt - guessArray.length;
+        $(".versuche span").text(versucheGesamt);
+        $(".versuche div").append(guessArray[i]);
+    }
+    return versucheGesamt;
 }
 
 function letterGuess() {
@@ -20,18 +31,19 @@ function letterGuess() {
     var matches = letters.includes(guess);
     if (matches) {
         $(".dashes div").html(underscoreify(secretWord, guessArray));
-    }   else if (guessCount == 1) {
-        $("img").attr("src", "");
-    }   else if (guessCount == 2) {
-        $("img").attr("src", "");
-    }   else if (guessCount == 3) {
-        $("img").attr("src", "");
-    }   else if (guessCount == 4) {
-        $("img").attr("src", "");
-    }   else if (guessCount == 5) {
-        $("img").attr("src", "");
-    }   else {
-        alert("Spieler 1 hat gewonnen!");
+    } else if (guessCount > 0 && answerArray <2) {
+        $("img").attr("src", "ersterVersuchFalsch.png");
+    } else if (guessCount == 2) {
+        $("img").attr("src", "zweiterVersuchFalsch.png");
+    } else if (guessCount == 3) {
+        $("img").attr("src", "dritterVersuchFalsch.png");
+    } else if (guessCount == 4) {
+        $("img").attr("src", "vierterVersuchFalsch.png");
+    }else if (guessCount == 5) {
+        $("img").attr("src", "fünfterVersuchFalsch.png");
+    } else if (guessCount > 5) {
+        alert("Spieler 2 hat gewonnen, tippe auf OK um ein neues Spiel zu starten");
+        location.reload();
     }
 }
 
@@ -57,7 +69,3 @@ $("#enterWord #submit-word").on("click", function(e) {
 });
 
 $(".alpha").children().on("click", letterGuess);
-
-function versucheUebrig() {
-
-}
