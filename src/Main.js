2a7000f21b172b65798c35bac6e7551a5e05df3d
Bonde.leon <leonbonde1@gmail.com>
2024-01-22 12:52:29 +0100

    alles bis auf server

--- src/Main.js
+++ src/Main.js
@@ -11,39 +11,34 @@ function getWord() {
         answerArray[i] = " _ \n";
         $(".dashes div").append(answerArray[i]);
     }
-    $(".versuche div").html(versucheUebrig());
-}
-
-function versucheUebrig() {
-    var versucheGesamt = secretWord.length + secretWord.length + 2;
-    for (i = versucheGesamt; i >= 0; i--) {
-        versucheGesamt = versucheGesamt - guessArray.length;
-        $(".versuche span").text(versucheGesamt);
-        $(".versuche div").append(guessArray[i]);
-    }
-    return versucheGesamt;
 }
 
 function letterGuess() {
     guess = $(this).attr('id');
     guessArray.push(guess);
     guessCount = guessArray.length;
-    var matches = letters.includes(guess);
-    if (matches) {
-        $(".dashes div").html(underscoreify(secretWord, guessArray));
-    } else if (guessCount > 0 && answerArray <2) {
-        $("img").attr("src", "ersterVersuchFalsch.png");
-    } else if (guessCount == 2) {
-        $("img").attr("src", "zweiterVersuchFalsch.png");
-    } else if (guessCount == 3) {
-        $("img").attr("src", "dritterVersuchFalsch.png");
-    } else if (guessCount == 4) {
-        $("img").attr("src", "vierterVersuchFalsch.png");
-    }else if (guessCount == 5) {
-        $("img").attr("src", "fünfterVersuchFalsch.png");
-    } else if (guessCount > 5) {
-        alert("Spieler 2 hat gewonnen, tippe auf OK um ein neues Spiel zu starten");
-        location.reload();
+    var versucheGesamt = 5;
+    for (i = versucheGesamt; i >= 0; i--);
+    {
+        versucheGesamt = versucheGesamt - guessCount;
+        $(".versuche span").text(versucheGesamt);
+        var matches = letters.includes(guess);
+        if (matches) {
+            $(".dashes div").html(underscoreify(secretWord, guessArray));
+        } else if (guessCount == 1) {
+            $("img").attr("src", "ersterVersuchFalsch.png");
+        } else if (guessCount == 2) {
+            $("img").attr("src", "zweiterVersuchFalsch.png");
+        } else if (guessCount == 3) {
+            $("img").attr("src", "dritterVersuchFalsch.png");
+        } else if (guessCount == 4) {
+            $("img").attr("src", "vierterVersuchFalsch.png");
+        } else if (guessCount == 5) {
+            $("img").attr("src", "fünfterVersuchFalsch.png");
+        } else if (guessCount >= 5) {
+            alert("Spieler 2 hat gewonnen, tippe auf OK um ein neues Spiel zu starten");
+            location.reload();
+        }
     }
 }
 
